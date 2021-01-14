b6db50b542247162f7510185c1a8a4cf1199f731
Grayton Savickas <gsavickaspro@gmail.com>
2021-01-14 10:01:06 -0600

    adds small formatting changes

---
+++ logic.js
@@ -0,0 +1,9 @@
+/*
+============================================
+; Title:  Functions with parameters
+; Author: Grayton Savickas, James Pinson, Jonathan Disla
+; Date:   14 Jan 2021
+; Modified By: 
+; Description: This is the main JavaScript for our single page project
+;===========================================
+*/
